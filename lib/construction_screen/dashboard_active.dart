d6732893853f84dfd3d2a9926bb8035ccb46b3ad
MD. Maruf <61429716+marufmon@users.noreply.github.com>
2023-02-06 22:02:06 +0600

    bottomsheetstyle

--- lib/construction_screen/dashboard_active.dart
+++ lib/construction_screen/dashboard_active.dart
@@ -42,15 +42,6 @@ class _DashboardActiveState extends State<DashboardActive> {
       projectAllDataModel = ProjectAllDataModel.fromJson(i);
       allData.add(projectAllDataModel!);
 
-      var suc = jsonDecode(responce.body);
-      print("api response with body:$suc");
-      if (suc["code"] != null) {
-        throw suc["message"];
-      }
-      for (var i in suc["data"]) {
-        projectAllDataModel = ProjectAllDataModel.fromJson(i);
-        allData.add(projectAllDataModel);
-      }
       print("All news length is ${allData}");
       return allData;
     }
