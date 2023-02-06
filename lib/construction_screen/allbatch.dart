e52d2a46f78e62ba357ca1d08014ef6aa793b901
MD. Maruf <61429716+marufmon@users.noreply.github.com>
2023-02-06 15:59:28 +0600

    bottomsheetstyle

--- lib/construction_screen/allbatch.dart
+++ lib/construction_screen/allbatch.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -9,31 +10,206 @@ class AllBatchPage extends StatelessWidget {
   TextEditingController batchNo = TextEditingController();
   TextEditingController soilType = TextEditingController();
   TextEditingController materialQuantity = TextEditingController();
-  TextEditingController receivingSite = TextEditingController();
+
+  final List position = [
+    'project co-ordinator',
+    'driver',
+  ];
+  String? selectedValue;
 
   @override
   Widget build(BuildContext context) {
     _showModalBottomSheet() {
       showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (BuildContext context) {
           return Container(
-            padding: EdgeInsets.all(10),
-            height: 500,
+            padding: EdgeInsets.all(10.0),
+            height: 600,
             color: Colors.white,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  child: Center(
-                    child: QrImage(
-                      gapless: true,
-                      version: QrVersions.auto,
-                      data: "Maruf",
-                      size: 200.0,
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(left: 120),
+                    child: Text(
+                      "Create Batch",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          fontFamily: "SFPro"),
                     ),
                   ),
+                  trailing: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: IconButton(
+                        onPressed: (() {
+                          Navigator.pop(context);
+                        }),
+                        icon: Icon(
+                          Icons.close,
+                          size: 20,
+                          color: Colors.grey,
+                        )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 140),
+                  height: 70,
+                  width: 70,
+                  child: QrImage(
+                    gapless: true,
+                    version: QrVersions.auto,
+                    data: "Maruf",
+                    size: 200.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Batch no",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      fontFamily: "SFPro"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: batchNo,
+                  decoration: InputDecoration(
+                    labelText: "786",
+                    filled: true,
+                    labelStyle: TextStyle(color: Colors.black),
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE1E1E1)),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Soil type",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      fontFamily: "SFPro"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: approvedBy,
+                  decoration: InputDecoration(
+                    labelText: "Just Soil",
+                    filled: true,
+                    labelStyle: TextStyle(color: Colors.black),
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE1E1E1)),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Material quantity",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      fontFamily: "SFPro"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: materialQuantity,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelStyle: TextStyle(color: Colors.black),
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE1E1E1)),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    labelText: "Material quantity",
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Receiving site",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      fontFamily: "SFPro"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                DropdownButtonFormField2(
+                  decoration: InputDecoration(
+                    //Add isDense true and zero Padding.
+                    //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    //Add more decoration as you want here
+                    //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+                  ),
+                  isExpanded: true,
+                  hint: const Text(
+                    'Select',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Color(0xff0086F0),
+                        fontFamily: "SFPro"),
+                  ),
+                  icon: const Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.black45,
+                  ),
+                  iconSize: 30,
+                  buttonHeight: 60,
+                  buttonPadding: const EdgeInsets.only(left: 20, right: 10),
+                  dropdownDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  items: position
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: const TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                  validator: (value) {
+                    if (value == null) {
+                      return 'Please select Your Position';
+                    }
+                  },
+                  onChanged: (value) {
+                    //Do something when changing the item if you want.
+                  },
+                  onSaved: (value) {
+                    selectedValue = value.toString();
+                  },
                 ),
                 Spacer(),
                 ElevatedButton(
@@ -72,7 +248,7 @@ class AllBatchPage extends StatelessWidget {
               _showModalBottomSheet();
             },
             child: Container(
-              width: 340,
+              width: 320,
               height: 52,
               child: Center(
                 child: Text(
