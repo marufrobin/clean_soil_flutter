d923c1319257ea99a19e08848687babac7a13482
Maruf Robin <marufrobin5@gmail.com>
2023-02-08 01:23:41 +0600

    signUp page new feild add and all body map data taken done

--- lib/authentication/account_signup.dart
+++ lib/authentication/account_signup.dart
@@ -33,7 +33,12 @@ class _AccountSignUpPageState extends State<AccountSignUpPage> {
     'project co-ordinator',
     'driver',
   ];
-  String? selectedValue;
+  final List userTypeList = [
+    'admin',
+    'employee',
+  ];
+  String? positionSelectedValue;
+  String? userTypeSelectedValue;
   adminRegistration() async {
     var adminRegUrl = "auth/user/register";
     Map map = Map<String, dynamic>();
@@ -46,9 +51,9 @@ class _AccountSignUpPageState extends State<AccountSignUpPage> {
     print("Company type ${map["userCompanyType"]}");
 
     // need to implement filed from UI page
-    map["userType"] = "cleansoil";
+    map["userType"] = userTypeSelectedValue;
 
-    map["userPosition"] = "project co-ordinator";
+    map["userPosition"] = positionSelectedValue;
     map["status"] = "invited";
     var responce = await http.post(
         Uri.parse("$baseUrl$apiVersionUrl$adminRegUrl"),
@@ -97,7 +102,7 @@ class _AccountSignUpPageState extends State<AccountSignUpPage> {
           child: Container(
             margin: EdgeInsets.all(16),
             // padding: EdgeInsets.all(16),
-            height: MediaQuery.of(context).size.height * 1,
+            height: MediaQuery.of(context).size.height * 1.1,
             width: double.infinity,
             child: Form(
               key: _formKey,
@@ -301,44 +306,67 @@ class _AccountSignUpPageState extends State<AccountSignUpPage> {
                     ),
                   ),
                   SizedBox(
-                    height: 8,
+                    height: 16,
                   ),
-                  Text(
-                    "User type",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 13,
-                        fontFamily: "SFPro"),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "user type is empty";
-                      }
-                    },
-                    controller: usertypeController,
+                  //user type button
+                  DropdownButtonFormField2(
                     decoration: InputDecoration(
-                      hintText: "Jane Doe",
-                      hintStyle: TextStyle(
+                      //Add isDense true and zero Padding.
+                      //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+                      isDense: true,
+                      contentPadding: EdgeInsets.zero,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      //Add more decoration as you want here
+                      //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+                    ),
+                    isExpanded: true,
+                    hint: const Text(
+                      'Select User Type',
+                      style: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 15,
-                          color: Color(0xffACACAC),
+                          color: Color(0xff0086F0),
                           fontFamily: "SFPro"),
-                      labelStyle: TextStyle(color: Colors.black),
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffE1E1E1)),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
                     ),
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black45,
+                    ),
+                    iconSize: 30,
+                    buttonHeight: 60,
+                    buttonPadding: const EdgeInsets.only(left: 20, right: 10),
+                    dropdownDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    items: userTypeList
+                        .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select Your Position';
+                      }
+                    },
+                    onChanged: (value) {
+                      //Do something when changing the item if you want.
+                    },
+                    onSaved: (value) {
+                      userTypeSelectedValue = value.toString();
+                    },
                   ),
                   SizedBox(
-                    height: 26,
+                    height: 16,
                   ),
+                  //position drop button
                   DropdownButtonFormField2(
                     decoration: InputDecoration(
                       //Add isDense true and zero Padding.
@@ -390,10 +418,12 @@ class _AccountSignUpPageState extends State<AccountSignUpPage> {
                       //Do something when changing the item if you want.
                     },
                     onSaved: (value) {
-                      selectedValue = value.toString();
+                      positionSelectedValue = value.toString();
                     },
                   ),
-                  Spacer(),
+                  SizedBox(
+                    height: 16,
+                  ),
                   ElevatedButton(
                       style:
                           ButtonStyle(elevation: MaterialStatePropertyAll(0)),
